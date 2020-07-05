99039344949de76887ce5c769ce5ea18cc12aa9c
Matthijs Boet <m.boet@chello.nl>
2020-07-05 16:34:12 +0200

    added uuid in app.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,19 @@ import React, { Component } from "react";
 import TodoList from './components/TodoList';
 import TodoInput from './components/TodoInput';
 
-import { v4 as uuidv4 } from "uuid";
+import { v5 as uuidv5 } from "uuid";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import './App.css';
+
+const v1options = {
+  node: [0x01, 0x23, 0x45, 0x67, 0x89, 0xab],
+  clockseq: 0x1234,
+  msecs: new Date("2011-11-01").getTime(),
+  nsecs: 5678,
+};
+uuidv5(v1options);
+console.log(v1options);
 
 export default class App extends Component {
   render() {
