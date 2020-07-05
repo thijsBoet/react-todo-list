696bd7f31d8c368a4094149c3264d0110cbcf5b3
Matthijs Boet <m.boet@chello.nl>
2020-07-05 17:01:58 +0200

    created state, passed them through via props created empty methods

--- src/App.js
+++ src/App.js
@@ -2,28 +2,59 @@ import React, { Component } from "react";
 import TodoList from './components/TodoList';
 import TodoInput from './components/TodoInput';
 
-import { v5 as uuidv5 } from "uuid";
+import { v4 as uuidv4 } from "uuid";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import './App.css';
 
-const v1options = {
-  node: [0x01, 0x23, 0x45, 0x67, 0x89, 0xab],
-  clockseq: 0x1234,
-  msecs: new Date("2011-11-01").getTime(),
-  nsecs: 5678,
-};
-uuidv5(v1options);
-console.log(v1options);
-
 export default class App extends Component {
+  state = {
+    items: [
+      {id:1, title: 'wake up'},
+      {id:2, title: 'do groceries'}
+    ],
+    id: uuidv4(),
+    item:'',
+    editItem: false
+  }
+
+  handleChange = e => {
+    console.log(e, 'change')
+  }
+  handleSubmit = e => {
+    console.log(e, 'submit')
+  }
+  clearList = () => {
+    console.log('clear')
+  }
+  handleDelete = id => {
+    console.log(`handle delete ${id}`)
+  }
+  handleEdit = id => {
+    console.log(`handle edit ${id}`)
+  }
+
   render() {
+    console.log(this.state)
     return (
-      <div className="App">
-        <div className="container">
-          <div className="row">
-            <TodoInput/><br/>
-            <TodoList/>
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mx-auto col-md-8 mt-4">
+            <h3 className="text-capitalize text-center">
+              <TodoInput
+                item={this.state.item}
+                handleChange={this.handleChange}
+                handleSubmit={this.handleSubmit}
+                handleEdit={this.state.handleEdit}
+              />
+
+              <TodoList
+                items={this.state.items}
+                clearList={this.clearList}
+                handleDelete={this.handleDelete}
+                handleEdit={this.state.handleEdit}
+              />
+            </h3>
           </div>
         </div>
       </div>
