acde5b12c6a858fec6bf428306b95c076b0a2485
Thijs Boet <m.boet@chello.nl>
2020-07-06 19:09:49 +0200

    Update TodoItem.js

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -7,13 +7,20 @@ export default class TodoItem extends Component {
       <li className="list-group-item text-capitalize d-flex justify-content-between my-2">
         <h6>{title}</h6>
         <div className="todo-icon">
-          <span className="mx-2 text-success" onClick={handleEdit}>
-            <i className="fas fa-pen" />
+          <span
+            className="mx-2 text-success"
+            onClick={handleEdit}
+            style={{ cursor: "pointer" }}
+          >
+            <i className="fas fa-pen icon" />
           </span>
-          <span className="mx-2 text-danger" onClick={handleDelete}>
-            <i className="fas fa-trash" />
+          <span
+            className="mx-2 text-danger"
+            onClick={handleDelete}
+            style={{ cursor: "pointer" }}
+          >
+            <i className="fas fa-trash icon" />
           </span>
-        </div>
       </li>
     );
   }
